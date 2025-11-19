cec60920accabdc581ba0b79767cc058205b9e06
Sureshraja02 <rsureshrajaa@gmail.com>
2025-11-19 18:05:29 +0530

    Add files via upload

--- aml-srv/src/main/java/com/fisglobal/fsg/core/aml/controller/RuleValidationController.java
+++ aml-srv/src/main/java/com/fisglobal/fsg/core/aml/controller/RuleValidationController.java
@@ -2,6 +2,7 @@ package com.fisglobal.fsg.core.aml.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,22 +12,32 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.fisglobal.fsg.core.aml.rule.process.request.RuleRequestVo;
 import com.fisglobal.fsg.core.aml.rule.process.response.RuleResponseVo;
+import com.fisglobal.fsg.core.aml.rule.service.RulesIdentifierService;
 import com.google.gson.Gson;
 
 @RestController
-@RequestMapping({ "/api/service/" })
+@RequestMapping({ "/api/v1/" })
 public class RuleValidationController {
 
 	private Logger LOGGER = LoggerFactory.getLogger(RuleValidationController.class);
 
-	@RequestMapping(value = { "/ruleprocess" }, method = { RequestMethod.POST })
+	@Autowired
+	RulesIdentifierService rulesIdentifierService;
+
+	@RequestMapping(value = { "fact/service" }, method = { RequestMethod.POST })
 	public ResponseEntity<?> ruleProcessMethod(@RequestBody RuleRequestVo requestObjParam) {
 
 		ResponseEntity<Object> retunRespEntity = null;
 		RuleResponseVo ruleResponseVoObj = null;
 		try {
 
-			retunRespEntity = getResponseEntity(new Gson().toJson(ruleResponseVoObj), HttpStatus.OK);
+			ruleResponseVoObj = rulesIdentifierService.toComputeAMLData(requestObjParam);
+			if (ruleResponseVoObj != null) {
+				retunRespEntity = getResponseEntity(new Gson().toJson(ruleResponseVoObj), HttpStatus.OK);
+			} else {
+				retunRespEntity = getResponseEntity("No Response Found", HttpStatus.OK);
+			}
+
 		} catch (Exception e) {
 			LOGGER.error("Exception found in RuleValidationController@ruleProcessMethod : {}", e);
 			retunRespEntity = getResponseEntity("Exception, Will check with Admin", HttpStatus.BAD_REQUEST);
